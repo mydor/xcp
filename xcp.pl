88fa23c7d69a5bba21a83fc3dcf98399b9c3f5e5
Michael Smith <archmage@arch-mage.com>
2020-12-03 19:50:13 -0600

    xcp#1 Add permission copy
    Add --preserve | --perms | -p permission copy
    
    Not sure how to copy Extended Attributes for now.  But, works for standard
    attributes and SELinux context
    
    ---
    
    Changes:
    ---------
    
    `xcp.pl`
    * Add option, and functions, to copy file permissions
    
    ---
    
    Testing:
    ---------
    
    None
    
    ---
    
    Branch: `xcp#1`

--- xcp.pl
+++ xcp.pl
@@ -2,17 +2,47 @@
 #
 
 use strict;
-use File::Basename;
-use Parallel::ForkManager;
-use POSIX;
-use Fcntl;
-use Getopt::Long;
 use Data::Dumper;
 use Digest::MD5 qw(md5_hex);
+use Fcntl;
+use File::Basename;
+use Getopt::Long;
+use Parallel::ForkManager;
+use POSIX;
 
 use constant K => 2**10;
 use constant M => 2**20;
 use constant G => 2**30;
+
+use constant STAT_DEV     => 0;
+use constant STAT_INODE   => 1;
+use constant STAT_MODE    => 2;
+use constant STAT_NLINK   => 3;
+use constant STAT_UID     => 4;
+use constant STAT_GID     => 5;
+use constant STAT_RDEV    => 6;
+use constant STAT_SIZE    => 7;
+use constant STAT_ATIME   => 8;
+use constant STAT_MTIME   => 9;
+use constant STAT_CTIME   => 10;
+use constant STAT_BLKSIZE => 11;
+use constant STAT_BLOCKS  => 12;
+
+use constant PW_NAME    => 0;
+use constant PW_PASS    => 1;
+use constant PW_UID     => 2;
+use constant PW_GID     => 3;
+use constant PW_QUOTA   => 4;
+use constant PW_COMMENT => 5;
+use constant PW_GECOS   => 6;
+use constant PW_DIR     => 7;
+use constant PW_SHELL   => 8;
+use constant PW_EXPIRE  => 9;
+
+use constant GR_NAME    => 0;
+use constant GR_PASS    => 1;
+use constant GR_GID     => 2;
+use constant GR_MEMBERS => 3;
 
 my %OPTS = (
   'concurrent' => 10,
@@ -31,12 +61,12 @@ GetOptions(
   'start-byte=i',
   'chunk-size=i',
   'block-size=i',
+  'preserve|perms|p',
+  'parent|path',
 );
 
 my ($src, $dst) = @ARGV;
 
-#my $src = '/mnt/drobo/';
-#my $dst = '/mnt/sdb/1/';
 my $ext = '';
 
 my $MAX_JOBS = $OPTS{'concurrent'};
@@ -58,11 +88,128 @@ my $base = basename("$dst");
 print "src: $src\n" if( $OPTS{'debug'} );
 print "dst: $dst\n" if( $OPTS{'debug'} );
 
-execute(
-   'mkdir',
-   '-p',
-   $dir
-);
+### execute(
+###    'mkdir',
+###    '-p',
+###    $dir
+### ) if $OPTS{'parent'};
+### 
+### foreach my $chunk ($OPTS{'start-chunk'}..$chunks) {
+###   my $pid = $pm->start and next;
+### 
+###   copyChunk( $src, "$dst$ext", $chunk, $src_size );
+### 
+###   $pm->finish;
+### }
+### 
+### $pm->wait_all_children;
+
+copyPerms( $src, "$dst$ext" ) if $OPTS{'preserve'};
+
+exit 0;
+
+sub modText {
+  my( $mode, $set ) = @_;
+
+  my $flags;
+  my $sid = 01;
+  my $sid_bits = ($mode & 07000) >> 9;
+
+  for(my $i=0; $i<3; $i++) {
+    my $oct = $mode & 07;
+
+    my $r = ( $oct & 04 ) ? 'r' : '-';
+    my $w = ( $oct & 02 ) ? 'w' : '-';
+    my $x = ( $oct & 01 ) ? 'x' : '-';
+
+    if ( $sid_bits & $sid ) {
+      my $s = ( $sid > 1 ) ? 's' : 't';
+      $x = ( $x eq '-' ) ? uc($s) : lc($s)
+    }
+    $flags = "$r$w$x$flags";
+
+    $sid <<= 1;
+    $mode >>= 3;
+  }
+
+  return $flags;
+}
+
+sub copyPerms {
+  my ( $src, $dst ) = @_;
+
+  my @stat = stat($src);
+
+  copyOwner( $src, $dst, @stat );
+  copyMode( $src, $dst, @stat );
+
+  copyContext( $src, $dst );
+  copyXattr( $src, $dst );
+
+  # Set the file times
+  utime $stat[STAT_ATIME],
+        $stat[STAT_MTIME],
+        $dst or die "$!: $dst";
+}
+
+sub copyOwner {
+  my ( $src, $dst, @stat ) = @_;
+
+  @stat = stat($src) if $#stat < 0;
+
+  # Set the file owner
+  # MUST BE FIRST, chown will clear setuid bits
+  my @pwuid = getpwuid($stat[STAT_UID]);
+  my @grgid = getgrgid($stat[STAT_GID]);
+  printf "Setting owner/group to '%s:%s' on %s\n",
+    $pwuid[PW_NAME],
+    $grgid[GR_NAME],
+    $dst
+    if $OPTS{'debug'};
+
+  chown $stat[STAT_UID],
+        $stat[STAT_GID],
+        $dst or die "$!: $dst";
+}
+
+sub copyMode {
+  my ( $src, $dst, @stat ) = @_;
+
+  @stat = stat($src) if $#stat < 0;
+
+  # Set the file mode
+  # MUST chown FIRST, chown will clear setuid bits
+  printf "Setting mode to 0%04o '%s' on %s\n",
+    $stat[STAT_MODE] & 07777,
+    modText( $stat[STAT_MODE] & 07777 ),
+    $dst if $OPTS{'debug'};
+
+  chmod $stat[STAT_MODE] & 07777,
+        $dst or die "$!: $dst";
+}
+
+sub copyXattr {
+  my ( $src, $dst ) = @_;
+
+  # Unknown how to do this, or if possible
+}
+
+sub copyContext {
+  my ( $src, $dst ) = @_;
+
+  # fugly but quick solution
+  open(GETCON, "ls --scontext \"$src\"|") or die $!;
+  my $context = <GETCON>;
+  close( GETCON );
+
+  chomp $context;
+  $context =~ s/\s.*$//;
+
+  print "Setting context '$context' on $dst\n" if $OPTS{'debug'};
+
+  my @args = ('chcon', $context, $dst);
+  system(@args) == 0 or die "system @args failed: $?"
+}
 
 sub copyChunk {
   my ( $src, $dst, $chunk, $size ) = @_;
@@ -76,16 +223,10 @@ sub copyChunk {
   my $remaining = $size - $start;
   $remaining = $OPTS{'chunk-size'} if $remaining > $OPTS{'chunk-size'};
 
-  #printf "Checking chunk %d at %d - %d\n", $chunk, $start, $start + $remaining - 1 if( $OPTS{'debug'} > 1 );
-
   if (isCopied( \*IN, \*OUT, $start, $remaining )) {
     printf "SKIPING chunk %d; %d - %d (%d)\n", $chunk, $start, $start + $remaining - 1, $remaining if $OPTS{'debug'} > 1;
     return;
   }
-
-###  printf "Copying chunk at %d - %d\n", $start, $start + $remaining - 1;
-
-### return if ( $start >= -s \*OUT ); # XXX
 
   sysseek(IN, $start, SEEK_SET);
   sysseek(OUT, $start, SEEK_SET);
@@ -100,8 +241,6 @@ sub copyChunk {
 
 sub copyBlocks {
   my ( $sFH, $dFH, $chunk, $size, $start, $remaining ) = @_;
-
-### return; # XXX
 
   my $cksum;
   my $data;
@@ -124,14 +263,6 @@ sub copyBlock {
   print "  $chunk - $blocks - $fstart - $bstart - $bremaining ($read)\n" if $OPTS{'debug'} > 2;
 
   syswrite(OUT, $data, $read) if $read;
-}
-
-foreach my $chunk ($OPTS{'start-chunk'}..$chunks) {
-  my $pid = $pm->start and next;
-
-  copyChunk( $src, "$dst$ext", $chunk, $src_size );
-
-  $pm->finish;
 }
 
 sub execute {
@@ -163,5 +294,4 @@ sub isCopied {
   return ( $sCksum eq $dCksum ) ? 1 : 0;
 }
 
-$pm->wait_all_children;
 
